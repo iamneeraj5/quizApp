b9977f0770dafa1ba148d0ed66ca07e3678f7fa5
Neeraj <neeraj.anithailango.ic@gmail.com>
2024-02-17 12:37:46 +0530

    Quiz App
    QuizAPP

---
+++ lib/data/questions.dart
@@ -0,0 +1,58 @@
+import 'package:quiz_app/models/quiz_ques.dart';
+
+const questions = [
+  QuizQuestion(
+    'What is the name given to the biennial international Test cricket series played between England and Australia?',
+    [
+      'The Ashes',
+      'Border-Gavaskar Trophy',
+      'The World Cup',
+      'Ranji Trophy',
+    ],
+  ),
+  QuizQuestion(
+    'What is the slang term given to a ball that is bowled so well that it is considered unplayable by the batsman?',
+    [
+      'A jaffa',
+      'Inswinger',
+      'Yorker',
+      'An over',
+    ],
+  ),
+  QuizQuestion(
+    'Who was the wicket-keeper of the Indian Cricket Team during the World Cup 2003 tournament?',
+    [
+      'Rahul Dravid',
+      'MS Dhoni',
+      'Nayan Mongia',
+      'Parthiv Patel',
+    ],
+  ),
+  QuizQuestion(
+    'Who has taken the most test wickets of all time?',
+    [
+      'Muttiah Muralitharan',
+      'Shane Warne',
+      'Wasim Akram',
+      'Kapil Dev',
+    ],
+  ),
+  QuizQuestion(
+    'Name the first Indian batsman to score 1,000 runs, in which most runs are in a calendar year and most fifties in the format?',
+    [
+      'Virat Kohli',
+      'Rohit Sharma',
+      'Shubman Gill',
+      'Sachin Tendulkar',
+    ],
+  ),
+  QuizQuestion(
+    'Which player holds the record of Highest ODI score by a wicketkeeper?',
+    [
+      'M S Dhoni',
+      'Adam Gilchrist',
+      'Jos Buttler',
+      'Kumar Sangakara',
+    ],
+  ),
+];
---
+++ lib/pages/quiz.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/pages/home_page.dart';
+import 'package:quiz_app/pages/questions_screen.dart';
+import 'package:quiz_app/pages/results_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  List<String> selectedAnswer = [];
+  var activeScreen = 'homepage';
+
+  
+  void restartQuiz() {
+    setState(() {
+      selectedAnswer = [];
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  void chooseAnswer(String answer) {
+    selectedAnswer.add(answer);
+
+    if (selectedAnswer.length == questions.length) {
+      setState(() {
+        activeScreen = 'results-screen';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget screenWidget = HomePage(switchScreen);
+    if (activeScreen == 'questions-screen') {
+      screenWidget = QuestionScreen(onSelectAnswer: chooseAnswer);
+    }
+    if (activeScreen == 'results-screen') {
+      screenWidget = ResultsScreen(
+        chosenAnswers: selectedAnswer,
+        onRestart: restartQuiz,
+      );
+    }
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: screenWidget,
+        ),
+      ),
+    );
+  }
+}
